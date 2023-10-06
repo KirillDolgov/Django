ea6e4e1e89c3090f7388b634d1628363cc4dcaf4
KirillDolgov <kir.iv.dol@gmail.com>
2023-10-06 21:28:53 +0300

    6.10.23

--- ProjectDjango/adventisements/app_advertisements/models.py
+++ ProjectDjango/adventisements/app_advertisements/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
 
 # Create your models here.
 class Advertisement(models.Model):
@@ -8,6 +11,24 @@ class Advertisement(models.Model):
     auction=models.BooleanField("Торг", help_text="Отметьте, если торг уместен")
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color:green; '
+                               'font-weight:bold">Сегодня в'
+                               ' {} </span>', created_time)
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color:green; '
+                               'font-weight:bold">Сегодня в'
+                               ' {} </span>', updated_time)
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
 
     class Meta:
         db_table = 'advertisements'
